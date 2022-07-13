6735200bed3f3838cea4d9feb34d3a7a7dd3278d
Sols7 <serg-lex@yandex.ru>
2022-07-13 09:23:35 +0300

    changed package git

--- src/main/java/ru/itsjava/git/MyClass.java
+++ src/main/java/ru/itsjava/git/MyClass.java
@@ -1,4 +1,7 @@
 package ru.itsjava.git;
 
 public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("I'm first best class");
+    }
 }
--- src/main/java/ru/itsjava/git/MyClass2.java
+++ src/main/java/ru/itsjava/git/MyClass2.java
@@ -1,4 +1,7 @@
 package ru.itsjava.git;
 
 public class MyClass2 {
+    public static void main(String[] args) {
+        System.out.println("I'm second best class");
+    }
 }
